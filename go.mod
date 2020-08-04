a8d0508d07f95a809bc56505488f54e8291c0e12
iamdavidzeng <15779712170@163.com>
2020-08-04 12:11:27 +0800

    Restructure with cobra

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/iamdavidzeng/gose64
 
 go 1.13
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.4.0
+)
--- main.go
+++ main.go
@@ -1,55 +1,24 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
-	"os"
+	"github.com/iamdavidzeng/gose64/cmd"
 )
 
-const encode = "--encode"
-const decode = "--decode"
-const hint = `For example1: gose64 --encode 'hello, world'.
-For exmaple2: gose64 --decode 'aGVsbG8sIHdvcmxk'.`
-
 func main() {
-	osArgs := os.Args
-	if len(osArgs) != 3 {
-		fmt.Println("gose64 need two arguments to execute.")
-		fmt.Println(hint)
-		return
-	}
-
-	firstArg := osArgs[1]
-
-	validFirstArg := []string{encode, decode}
-	isFirstArgValid := false
-	for _, item := range validFirstArg {
-		if item == firstArg {
-			isFirstArgValid = true
-		}
-	}
-	if !isFirstArgValid {
-		fmt.Printf("Invalid command arguments: %v\n", firstArg)
-		fmt.Println("gose64 first argument only support --decode or --encode.")
-		fmt.Println(hint)
-		return
-	}
-
-	stringWaitToHandle := osArgs[2]
-	if stringWaitToHandle == "" {
-		fmt.Println("gose64 second argument can't be empty.")
-		fmt.Println(hint)
-		return
-	}
-	if firstArg == encode {
-		encoded := base64.StdEncoding.EncodeToString([]byte(stringWaitToHandle))
-		fmt.Println(encoded)
-		return
-	}
-	decoded, err := base64.StdEncoding.DecodeString(stringWaitToHandle)
-	if err != nil {
-		fmt.Println("decode error:", err)
-	}
-	fmt.Println(string(decoded))
-	return
+	cmd.Execute()
 }
